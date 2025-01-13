b8c0076eb7ddb2208d611e0a8d6ae198b19199f9
Aneesh165 <aneeshmp165@gmail.com>
2025-01-13 09:59:19 +0530

    app

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import Card from "./components/Card";
+export default function App() {
+  return (
+    <div className="w-full h-screen flex justify-center items-center">
+      <Card/>
+    </div>
+  )
+}
